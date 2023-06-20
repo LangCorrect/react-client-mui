9a57d07cd9e2c72e0d391e07b8a6eabb09b1caef
keyxcode <keyxcode@gmail.com>
2023-06-20 18:42:19 +0700

    Make sidebar sticky

--- src/pages/ProfileDetailPage.tsx
+++ src/pages/ProfileDetailPage.tsx
@@ -31,12 +31,19 @@ const ProfileDetailPage = ({
         <>
             <BootstrapContainer>
                 <Grid container>
+                    {/* Main content */}
                     <Grid
                         item
                         xs={12}
                         md={9}
                         order={{ xs: 2, md: 1 }}
-                        paddingRight={3}
+                        paddingRight={{ xs: 0, md: 3 }}
+                        sx={{
+                            borderRight: (theme: Theme) => ({
+                                xs: 0,
+                                md: `1px solid ${theme.palette.divider}`,
+                            }),
+                        }}
                     >
                         <UserContent
                             user={user}
@@ -45,18 +52,20 @@ const ProfileDetailPage = ({
                             handleTabChange={handleTabChange}
                         />
                     </Grid>
+                    {/* User info/ sidebar */}
                     <Grid
                         item
                         xs={12}
                         md={3}
                         order={{ xs: 1, md: 2 }}
                         sx={{
-                            borderLeft: (theme: Theme) => ({
-                                xs: 0,
-                                md: `1px solid ${theme.palette.divider}`,
-                            }),
+                            position: "sticky",
+                            top: 0,
+                            height: "fit-content",
+                            bgcolor: "background.default",
                         }}
                         paddingLeft={{ xs: 0, md: 3 }}
+                        paddingBottom={{ xs: 3, md: 0 }}
                     >
                         <UserInfo user={user} />
                     </Grid>
